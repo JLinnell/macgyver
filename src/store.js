7d49520fa83502237a4cdfd713c9d3cc6bcc5675
JLinnell <linnelljudith@gmail.com>
2019-03-27 18:00:50 -0400

    updated links

--- src/store.js
+++ src/store.js
@@ -3,7 +3,6 @@ import thunk from 'redux-thunk';
 import userReducer from './reducers/userReducer';
 import hackReducer from './reducers/hackReducer';
 
-//rootReducer is pink sausage and hack/user are little blocks
 
 const rootReducer = combineReducers({
     user: userReducer,
